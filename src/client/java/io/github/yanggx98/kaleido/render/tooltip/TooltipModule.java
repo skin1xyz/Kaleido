80ce254762353597903380a33ca916f619fe399d
yanggx98 <yanggx98@outlook.com>
2024-05-26 21:52:37 +0800

    1.1.0

--- src/client/java/io/github/yanggx98/kaleido/render/tooltip/TooltipModule.java
+++ src/client/java/io/github/yanggx98/kaleido/render/tooltip/TooltipModule.java
@@ -4,14 +4,20 @@ import io.github.yanggx98.kaleido.IKaleidoModule;
 import io.github.yanggx98.kaleido.render.tooltip.impl.TooltipItemStackCache;
 import io.github.yanggx98.kaleido.render.tooltip.impl.TooltipMark;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.client.item.TooltipType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+
+import java.util.List;
 
 public class TooltipModule implements IKaleidoModule {
 
     @Override
     public void load() {
-        ItemTooltipCallback.EVENT.register(((stack, context, lines) -> {
+        ItemTooltipCallback.EVENT.register((stack, tooltipContext, tooltipType, lines) -> {
             TooltipItemStackCache.saveItemStack(stack);
             lines.add(TooltipMark.createItemMark(stack));
-        }));
+        });
     }
 }
