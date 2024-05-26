4e5f9765b8e468b45f0473f0d76512c7e73b2d6c
yanggx98 <yanggx98@outlook.com>
2024-05-26 23:33:18 +0800

    修复生物属性mixin可能会与其他mod冲突的bug

--- src/main/java/io/github/yanggx98/kaleido/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/yanggx98/kaleido/mixin/LivingEntityMixin.java
@@ -10,10 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "createLivingAttributes", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "createLivingAttributes", at = @At("RETURN"))
     private static void injectCreateLivingAttributes(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
         DefaultAttributeContainer.Builder builder = cir.getReturnValue();
         ILivingEntityAttributeAddition.EVENT.invoker().addAttribute(builder);
-        cir.setReturnValue(builder);
     }
 }
